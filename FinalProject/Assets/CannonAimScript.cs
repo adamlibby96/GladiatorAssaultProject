730769220610883f6a058dd762be991d364122af
Adam Libby <adam.libby@eagles.oc.edu>
2018-04-10 22:43:25 -0500

    Not sure what I did
    Post from yesterday I forgot to upload. I am not entirely sure what I changed

---
+++ FinalProject/Assets/CannonAimScript.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonAimScript : MonoBehaviour {
+
+    [SerializeField] private Transform origPos;
+    [SerializeField] private Transform aimPos;
+    [SerializeField] private float duration = 1f;
+    public bool canAim { get; set; }
+    private bool isAim = false;
+    private bool aiming = false;
+    private float elapsed = 0f;
+
+    private void Start()
+    {
+        transform.position = origPos.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canAim)
+        {
+            if (Input.GetMouseButtonDown(1) && !aiming)
+            {
+                if (!isAim)
+                {
+                    StartCoroutine(cannonAimer(duration));
+                }
+                else
+                {
+                    StartCoroutine(cannonAimer(duration));
+                }
+            }
+        }
+    }
+
+    private IEnumerator cannonAimer(float dur)
+    {
+        aiming = true;
+        float elapsed = 0f;
+        while (elapsed <= dur)
+        {
+            float pct = elapsed / dur;
+            if (isAim)
+            {
+                transform.position = Vector3.Lerp(aimPos.position, origPos.position, pct);
+            }
+            else
+            {
+                transform.position = Vector3.Lerp(origPos.position, aimPos.position, pct);
+            }
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+        isAim = !isAim;
+        aiming = false;
+    }
+
+}
